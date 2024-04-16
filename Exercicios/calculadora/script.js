5e30edac29b76af3795385f11a6e13c7c278793f
Matheus Victor Machado Bernardes <118033166+Matupinhos@users.noreply.github.com>
2024-04-16 09:24:03 -0300

    Add files via upload

--- Exercicios/calculadora/script.js
+++ Exercicios/calculadora/script.js
@@ -1,28 +1,24 @@
 function calcular() {
-    var numero1 = parseInt(document.getElementById('numero1').value);
-    var numero2 = parseInt(document.getElementById('numero2').value);
+    var numero1 = parseInt(document.getElementById('num1').value);
+    var numero2 = parseInt(document.getElementById('num2').value);
     var operacao = document.getElementById('escolha').value;
     var texto = document.querySelector(".resultado")
     var resultado;
     switch (operacao) {
-        case 'escolhaOpercao':
+        case 'escolha':
             alert("Opção inválida! Escolha outra opção.");
             break;
         case 'Adicao':
             resultado = numero1 + numero2;
-            console.log(resultado);
             break;
         case 'Subtracao':
             resultado = numero1 - numero2;
-            console.log(resultado);
             break;
         case 'Multiplicacao': 
             resultado = numero1 * numero2;
-            console.log(resultado);
             break;
         case 'Divisao':
             resultado = numero1 / numero2;
-            console.log(resultado);
             break;
     }
     texto.textContent = resultado;
