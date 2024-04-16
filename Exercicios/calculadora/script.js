e11661dd4b2e94ac2a8c753b3c8bdd32cec06de6
Matheus Victor Machado Bernardes <118033166+Matupinhos@users.noreply.github.com>
2024-04-16 11:25:27 -0300

    Add files via upload

--- Exercicios/calculadora/script.js
+++ Exercicios/calculadora/script.js
@@ -1,3 +1,21 @@
+function sinal() {
+    var operacao = document.getElementById('escolha').value;
+    var texto = document.querySelector('.sinal');
+    switch (operacao) {
+        case 'Adicao':
+            texto.innerHTML = '+';
+            break
+        case 'Subtracao':
+            texto.innerHTML = '-';
+            break
+        case 'Multiplicacao':
+            texto.innerHTML = '*';
+            break
+        case 'Divisao':
+            texto.innerHTML = '/'; 
+            break
+    }
+}
 function calcular() {
     var numero1 = parseInt(document.getElementById('num1').value);
     var numero2 = parseInt(document.getElementById('num2').value);
@@ -18,8 +36,12 @@ function calcular() {
             resultado = numero1 * numero2;
             break;
         case 'Divisao':
+            if (numero2 === 0) {
+                alert('Coloque outro número')
+                break
+            }
             resultado = numero1 / numero2;
             break;
     }
-    texto.textContent = resultado;
+    texto.textContent = resultado;/*Mostra a variavél resultado no documento HTML*/ 
 }
